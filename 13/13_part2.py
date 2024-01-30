281fa66ad8c1670487bb136c4c59b274a90d34b0
Michael Bazzanti <michael6993@msn.com>
2024-01-30 22:37:01 +0100

    13 part 2 py

--- 13/13_part2.py
+++ 13/13_part2.py
@@ -14,25 +14,9 @@ test = """#.##..##.
 #####.##.
 #####.##.
 ..##..###
-#....#..#
+#....#..#"""
 
-####.##.#..##.#
-..#...##..#####
-.##..#.......#.
-.##..#.......#.
-..#...##..#####
-####.##.#..##.#
-####...#..#....
-....###.###.###
-..###.#.#.##.##
-.####.#.######.
-.####.#.###.##.
-..###.#.#.##.##
-....###.###.###
-####...#..#....
-####.##.#..##.#"""
-
-def day13_part1Func(input: list):
+def day13_part2Func(input: list):
     result = 0
     lines = input.split("\n\n")
 
@@ -60,23 +44,38 @@ def checkMap(input: list):
 
     return result
 
+def stringDistance(item1: str, item2: str) -> bool:
+    if item1 == item2:
+        return 0
+
+    counter = 0
+    for i in range(len(item1)):
+        if item1[i] != item2[i]:
+            counter += 1
+
+    return counter
+
 def checkMirror(mapp: list):
     result = 0
     for i in range(len(mapp) - 1):
-
-        if mapp[i] == mapp[i+1]:           
-           for j in range(min(i+1, len(mapp) - i)): # check if valid mirror
-               if mapp[i-j] != mapp[i+j+1]:
-                   break
-               # at the end of the loop return the index of the mirror + 1 = number of rows
-               if i+j+1 == len(mapp) - 1 or i-j == 0:
-                   return i+1
+        diffCounter = 0
+        for j in range(min(i+1, len(mapp) - i)): # check if valid mirror
+            diffCounter += stringDistance(mapp[i-j], mapp[i+j+1])
+            if diffCounter > 1:
+                break
+            
+            # at the end of the loop return the index of the mirror + 1 = number of rows
+            if (i+j+1 == len(mapp) - 1 or i-j == 0):
+                if( diffCounter == 1):
+                    return i+1
+                else:
+                    break
 
     return result
 
-print(day13_part1Func(test)) # 705 - base=405
+print(day13_part2Func(test)) # base=400
 
 # read and test file input
 file = open("input.txt", "r")
 input = file.read()
-print(day13_part1Func(input)) # 
+print(day13_part2Func(input)) #  
